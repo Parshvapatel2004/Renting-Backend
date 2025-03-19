27e55e5daee9033b0eb345a1b02afb19fa3eb049
Parshva <codewithpasshu@gmail.com>
2025-03-19 11:47:18 +0530

    Changes

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const dbConnect=require('./DB/db.Connect');
+const dbConnect = require("./DB/db.Connect");
 const express = require("express");
 const cors = require("cors");
-const { AddContactus } = require('./API/AddContactus');
-const { Register } = require('./API/Register');
-
+const { AddContactus } = require("./API/AddContactus");
+const { Register } = require("./API/Register");
+const { Login } = require("./API/Login");
+const { FetchAllUser } = require("./API/FetchAllData");
 
 dbConnect();
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 // create route
 
-app.post("/AddContactus",AddContactus);
-app.post("/Register",Register);
+app.post("/AddContactus", AddContactus);
+app.post("/Register", Register);
+app.post("/Login", Login);
+app.get("/fetchalluser", FetchAllUser);
 
 const PORT = 8000;
 app.listen(PORT);
